467d7f3c14eb330ebabeb1a09e9a7b0df1593bba
Steve Nono <ghistev@yahoo.fr>
2019-04-29 16:36:44 +0200

    Added Pop-up messages for the different levels

--- Assets/BarrierLogic.cs
+++ Assets/BarrierLogic.cs
@@ -7,10 +7,18 @@ public class BarrierLogic : MonoBehaviour
 {
     public GameObject bomb;
     public GameObject Message1;
+    public GameObject Message2;
+    public GameObject Message3;
+
+    public GameObject Barrier;
+    private float hitCounter;
+    private float hitCounter2;
+    private float hitCounter3;
 
     private void Start()
     {
         bomb.SetActive(true);
+
 
     }
 
@@ -18,27 +26,51 @@ public class BarrierLogic : MonoBehaviour
     {
         if(other.CompareTag("Barrier1"))
         {
-            Debug.Log("Contact made with the 1st Barrier");
-            bomb.SetActive(false);
-            Message1.SetActive(true);
-            StartCoroutine(wait(5F));
-            Debug.Log("Show msg for 5s");
+            hitCounter++;
+            Debug.Log("Counter started");
+            if(hitCounter == 1)
+            {
+                Debug.Log("Contact made with the 1st Barrier: " + hitCounter);
+                bomb.SetActive(false);
+                Message1.SetActive(true);
+                StartCoroutine(wait(5F));
+                Debug.Log("Show msg for 5s");
+            }
 
 
 
         }
 
-        //if (other.CompareTag("Barrier2"))
-        //{
-        //    Debug.Log("Contact made with the 2nd Barrier");
-        //    bomb.SetActive(true);
-        //}
 
-        //if (other.CompareTag("Barrier3"))
-        //{
-        //    Debug.Log("Contact made with the 3rd Barrier");
-        //    bomb.SetActive(false);
-        //}
+        if (other.CompareTag("Barrier2"))
+        {
+            hitCounter2++;
+            Debug.Log("Counter started at barrier 3: " + hitCounter2);
+            if (hitCounter2 == 1)
+            {
+                Debug.Log("Contact made with the 3rd Barrier");
+                bomb.SetActive(true);
+                Message2.SetActive(true);
+                StartCoroutine(wait(5F));
+                Debug.Log("Show msg for 5s");
+            }
+        }
+
+        if (other.CompareTag("Barrier3"))
+        {
+            hitCounter3++;
+            Debug.Log("Counter started at barrier 3: " + hitCounter3);
+            if (hitCounter3 == 2)
+            {
+                Debug.Log("Contact made with the 3rd Barrier");
+                bomb.SetActive(true);
+                Message3.SetActive(true);
+                StartCoroutine(wait(5F));
+                Debug.Log("Show msg for 5s");
+            }
+        }
+
+
 
 
     }
@@ -51,6 +83,10 @@ public class BarrierLogic : MonoBehaviour
             yield return new WaitForSeconds(delay);
             //Debug.Log("Delay ends");
             Message1.SetActive(false);
+            Message2.SetActive(false);
+            Message3.SetActive(false);
+            StartCoroutine(wait(5F));
+
         }
     }
 
